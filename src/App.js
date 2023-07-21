d4e95cc27729022a2bb8f9a2a8ed8870363308e5
lsdev200 <lsdev200@gmail.com>
2023-07-21 15:21:40 +0530

    21-7-23

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ export default function App() {
 	const [tasks, setTasks] = useState([]);
 	const [opened, setOpened] = useState(false);
 
+
+	
+
 	const preferredColorScheme = useColorScheme();
 	const [colorScheme, setColorScheme] = useLocalStorage({
 		key: 'mantine-color-scheme',
